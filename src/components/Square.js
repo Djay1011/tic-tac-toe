8e89d0fe628e4c5964f0ef1537e33c7b332f6c77
Emmanuel Chinakwe <chinakwemmanuel@icloud.com>
2025-03-22 19:05:39 +0000

    Added animating moves using framer motion

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion"
 
 const Square = ({ value, onClick }) => {
   return (
-    <button className="square" onClick={onClick}>
+    <motion.button className="square"
+      onClick={onClick}
+      whileTap={{ scale: 0.9 }}
+      animate={{ opacity: value ? 1 : 0.5, scale: value ? 1.1 : 1 }}
+      transition={{ duration: 0.3 }}>
       {value}
-    </button>
+    </motion.button>
   );
 };
 
